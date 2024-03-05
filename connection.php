40ebe55fc95a2f1284c8c664ccb4362f53b8345b
Bariquit, John Ivan <143580280+ivangwapo@users.noreply.github.com>
2024-03-05 12:46:25 +0000

    uploading database

--- connection.php
+++ connection.php
@@ -1,6 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>The Man Cave Sales Monitoring System</title>
+</head>
+<body>
+  <?php
+     
 
-echo "<h1> Hello World </h1>";
+      $hostname = "localhost";
+      $username = "root";
+      $password = "";
+      $database = "mariadb";
 
+      $connection = mysqli_connect($hostname, $username, $password, $database);
 
-?>
+      /*
+        Create an if-else statement to test if the connection is successful or not.
+      */
+      if($connection) {
+        echo "Connected to database";
+      } else {
+        echo "Failed to connect";
+      }
+  ?>
+</body>
+</html>
